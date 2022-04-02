7fd9e9851b2b568f7b1b7e3c12033a93a457f3f0
Hannan Yousuf <hannanyousuf@Hannans-MacBook-Pro.local>
2022-04-02 15:06:31 +0500

    Two way data binding

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-server',
+  templateUrl: './server.component.html',
+  styleUrls: ['./server.component.scss']
+})
+export class ServerComponent implements OnInit {
+  serverId:number = 10;
+  serverStatus:string = 'Offline';
+
+  getServerStatus(){
+    return this.serverStatus;
+  }
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
